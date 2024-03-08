4158e80022a3eb5ad0c2a979ac7f5d0f2b2550e2
filipjanevski27 <filipjanevski@oakland.edu>
2024-03-07 23:11:36 -0500

    progress made

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
+//import usedCars from usedCars.js
+import usedCars from "./usedCars.js";
+
 //connect input form from html to js
 const form = document.getElementById("search-form");
 
 //retrieve user data
-const minYear = document.getElementById("min-year-input");
-const maxYear = document.getElementById("max-year-input");
+const minYear = parseInt(document.getElementById("min-year-input").value);
+const maxYear = parseInt(document.getElementById("max-year-input").value);
 
 const toyotaBox = document.getElementById("make-toyota");
 const hondaBox = document.getElementById("make-honda");
@@ -31,10 +34,10 @@ const blueBox = document.getElementById("color-blue");
 const redBox = document.getElementById("color-red");
 const greenBox = document.getElementById("color-green");
 
-const maxMiles = document.getElementById("miles-input");
+const maxMiles = parseInt(document.getElementById("miles-input").value);
 
-const minPrice = document.getElementById("min-price-input");
-const maxPrice = document.getElementById("max-price-input");
+const minPrice = parseInt(document.getElementById("min-price-input").value);
+const maxPrice = parseInt(document.getElementById("max-price-input").value);
 
 //element for embedded results
 const result = document.getElementById("result");
@@ -63,6 +66,8 @@ function createCarCard(car) {
       </div>
     </div>
   `;
+
+  return cardTemplate;
 }
 
 //reset page upon new user search
@@ -72,13 +77,145 @@ function clearResults() {
 
 //display the dynamic content with car results
 function showResults(item) {
-  const newContent = item.map(createCarCard).join(""); //.join for space between cards
-  result.innerHTML += newContent || "<p> No Results Found. Search Again. </p>";
+  for (i = 0; i < item.length; i++) {
+    result.innerHTML += createCarCard(item[i]);
+  }
 }
+
+//functionality behind filter searching
+function handleSearch(e) {
+  //empty array to add filtered cars
+  const filteredCars = [];
+  isSearching = true;
+
+  //loop through all elements of usedCars
+  for (i = 0; i < usedCars.length; i++) {
+    const car = usedCars[i];
+    const isMake = true;
+    const isColor = true;
+
+    //check make
+    switch (car.make) {
+      case "Toyota" && !toyotaBox.checked:
+        isMake = false;
+        break;
+      case "Honda" && !hondaBox.checked:
+        isMake = false;
+        break;
+      case "Ford" && !fordBox.checked:
+        isMake = false;
+        break;
+      case "Nissan" && !nissanBox.checked:
+        isMake = false;
+        break;
+      case "Chevrolet" && !chevroletBox.checked:
+        isMake = false;
+        break;
+      case "Volkswagen" && !volkswagenBox.checked:
+        isMake = false;
+        break;
+      case "Hyundai" && !hyundaiBox.checked:
+        isMake = false;
+        break;
+      case "Subaru" && !subaruBox.checked:
+        isMake = false;
+        break;
+      case "Mazda" && !mazdaBox.checked:
+        isMake = false;
+        break;
+      case "Kia" && !kiaBox.checked:
+        isMake = false;
+        break;
+      case "Dodge" && !dodgeBox.checked:
+        isMake = false;
+        break;
+      case "Cadillac" && !cadillacBox.checked:
+        isMake = false;
+        break;
+      case "Jaguar" && !jaguarBox.checked:
+        isMake = false;
+        break;
+      case "Tesla" && !teslaBox.checked:
+        isMake = false;
+        break;
+      case "Porsche" && !porscheBox.checked:
+        isMake = false;
+        break;
+      case "Lexus" && !lexusBox.checked:
+        isMake = false;
+        break;
+      case "BMW" && !bmwBox.checked:
+        isMake = false;
+        break;
+    }
+
+    //check color
+    switch (car.color) {
+      case "Silver":
+        if (!silverBox.checked) {
+          isColor = false;
+        }
+        break;
+      case "White":
+        if (!whiteBox.checked) {
+          isColor = false;
+        }
+        break;
+      case "Black":
+        if (!blackBox.checked) {
+          isColor = false;
+        }
+        break;
+      case "Gray":
+        if (!grayBox.checked) {
+          isColor = false;
+        }
+        break;
+      case "Blue":
+        if (!blueBox.checked) {
+          isColor = false;
+        }
+        break;
+      case "Red":
+        if (!redBox.checked) {
+          isColor = false;
+        }
+        break;
+      case "Green":
+        if (!greenBox.checked) {
+          isColor = false;
+        }
+        break;
+    }
+
+    //check if text inputs are empty
+
+    //check all filters
+    if (
+      minYear <= car.year &&
+      car.year <= maxYear &&
+      isMake &&
+      isColor &&
+      car.mileage <= maxMiles &&
+      minPrice <= car.price &&
+      car.price <= maxPrice
+    ) {
+      filteredCars.push(car);
+    }
+  }
+
+  //show results of filtered cars
+  showResults(filteredCars);
+}
+
+//event listener
+form.addEventListener("submit", handleSearch);
 
 //main function - init
 async function init() {
+  result.innerHTML = `<p>javascript file works at least</p>`;
   clearResults();
+  isSearching = false;
   showResults(usedCars);
 }
 
